f9bb8f7e1bb8824b7694a2bed05faa24fadc0d68
Joanne Kus <kusjoanne@gmail.com>
2020-06-04 21:59:56 -0500

    update Footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer(){
   return  <div className='footer'>
     © 2020 Joanne Kus
-    <a href="https://twitter.com/jkus_dev" target="_blank"><img class='socialMediaIcon' alt='twitter-icon' src='https://image.flaticon.com/icons/svg/733/733579.svg'></img></a>
-    <a href="https://github.com/kusjoanne" target="_blank"><img class='socialMediaIcon' alt='github-icon' src='https://image.flaticon.com/icons/svg/733/733553.svg'></img></a>
+    <a href="https://twitter.com/jkus_dev" target="_blank"><img className='socialMediaIcon' alt='twitter-icon' src='https://image.flaticon.com/icons/svg/733/733579.svg'></img></a>
+    <a href="https://github.com/kusjoanne" target="_blank"><img className='socialMediaIcon' alt='github-icon' src='https://image.flaticon.com/icons/svg/733/733553.svg'></img></a>
   </div>
 }
 
