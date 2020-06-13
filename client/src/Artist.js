91a531702cf1a5ca49c7ca963b756782c153499b
jkus <kusjoanne@gmail.com>
2020-06-13 17:50:12 -0500

    updated results3

--- client/src/Artist.js
+++ client/src/Artist.js
@@ -12,32 +12,32 @@ function Artist(props){
       width: '12rem',
     }
   }
-
-  // <div class="col-sm-6 col-lg-3 py-2">
-  //     <div class="card h-100">
-  //         <img class="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap"></img>
-  //         <div class="card-body">
-  //             <h4 class="card-title">Card title</h4>
-  //             <p class="card-text">This is a longer card with supporting text below as a natural.</p>
-  //             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  //         </div>
+  // <div className="deviceCard col-3 mb-3" style={styles.flipInX}>
+  //   <Card style={{ width: '12rem' }}>
+  //     <div className="cardImg">
+  //       <Card.Img className='albumArt' variant="top" src={props.result.albumArt} />
+  //       <div className="middle">
+  //         <img className="playbtn" alt="playButton" src="https://image.flaticon.com/icons/png/512/907/907754.png"></img>
+  //       </div>
   //     </div>
+  //     <Card.Body>
+  //       <Card.Title style={styles.flipInX}>{props.result.artistName}</Card.Title>
+  //       {props.result.hasOwnProperty('trackName')&& <Card.Subtitle className="mb-2 text-muted cardTitle">{props.result.trackName}</Card.Subtitle>}
+  //     </Card.Body>
+  //   </Card>
   // </div>
+
   //onClick={props.playAudio.bind(this,props.result.previewUrl)}
   return <StyleRoot>
-    <div className="deviceCard col-3 mb-3" style={styles.flipInX}>
-      <Card style={{ width: '12rem' }}>
-        <div className="cardImg">
-          <Card.Img className='albumArt' variant="top" src={props.result.albumArt} />
-          <div className="middle">
-            <img className="playbtn" alt="playButton" src="https://image.flaticon.com/icons/png/512/907/907754.png"></img>
-          </div>
+    <div class="col-sm-6 col-lg-3 py-2">
+        <div class="card h-100">
+            <img class="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap"></img>
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
         </div>
-        <Card.Body>
-          <Card.Title style={styles.flipInX}>{props.result.artistName}</Card.Title>
-          {props.result.hasOwnProperty('trackName')&& <Card.Subtitle className="mb-2 text-muted cardTitle">{props.result.trackName}</Card.Subtitle>}
-        </Card.Body>
-      </Card>
     </div>
   </StyleRoot>
 }
