791d8d3f664bac5c65eb01143285d68a8dbfc538
jkus <kusjoanne@gmail.com>
2020-06-13 19:36:19 -0500

    test8

--- client/src/Artist.js
+++ client/src/Artist.js
@@ -20,8 +20,8 @@ function Artist(props){
   // <div className="deviceCard col-3 mb-3" style={styles.flipInX}>
 
   return <StyleRoot>
-    <Col md={4}
-      <Card style={{ width: '12rem' }}>
+    <Col md={4}>
+      <Card style={styles.flipInX}>
         <div className="cardImg">
           <Card.Img className='albumArt' variant="top" src={props.result.albumArt} />
           <div className="middle">
