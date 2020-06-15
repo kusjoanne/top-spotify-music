125721d415737859b82af28fe0e8c61eb36bf65d
Joanne Kus <kusjoanne@gmail.com>
2020-06-15 13:16:49 -0500

    moved audio2

--- client/src/Results.js
+++ client/src/Results.js
@@ -4,14 +4,34 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 function Results(props){
+  let songlink = null;
+  let audio = props.audio;
 
+  function playAudio(audiolink){
+    //if the song clicked is the same as the song that was playing
+    if(audiolink == songlink){
+       if (audio.paused){
+        audio.play();
+      } else{
+        audio.pause();
+      }
+    //if the song clicked is different than the one that was previously clicked
+  } else {
+      if(audio){
+        audio.pause();
+      }
+      songlink = audiolink;
+      audio = new Audio(audiolink);
+      audio.play();
+    }
+  }
 
 
   return <Container>
     <Row className="show-grid">
       {props.results.map( (result, index) => {
         return <Artist key={index} result={result} playAudio={props.playAudio}/>;
-     })}
+      })}
     </Row>
   </Container>
 }
